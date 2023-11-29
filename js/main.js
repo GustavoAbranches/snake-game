5afae30222531b75050413b150a223579c44a42f
Gustavo Abranches <gusabranches@gmail.com>
2023-11-29 20:22:01 -0300

    projeto finalizado

--- js/main.js
+++ js/main.js
@@ -1,12 +1,53 @@
 const canvas = document.querySelector('canvas')
 const ctx = canvas.getContext('2d')
-const size = 30
 
-const snake = [
-  { x: 200, y: 200 },
-  { x: 230, y: 200 }
-]
-let direction = "left"
+const score = document.querySelector('.score--value')
+const finalScore = document.querySelector('.final-score > span')
+const menu = document.querySelector('.menu-screen')
+const buttonPlay = document.querySelector('.btn-play')
+
+const audio = new Audio('../assets/audio.mp3')
+const size = 30
+const initialPosition = { x: 270, y: 240 }
+let snake = [initialPosition]
+
+const incrementScore = () => {
+  score.innerText = parseInt(score.innerText) + 10
+}
+
+const randomNumber = (min, max) => {
+  return Math.round(Math.random() * (max - min) + min)
+}
+
+const randomPosition = () => {
+  const number = randomNumber(0, canvas.width - size)
+  return Math.round(number / 30) * 30
+}
+
+const randomColor = () => {
+  const red = randomNumber(0, 255)
+  const green = randomNumber(0, 255)
+  const blue = randomNumber(0, 255)
+
+  return `rgb(${red}, ${green}, ${blue})`
+}
+
+const food = {
+  x: randomPosition(),
+  y: randomPosition(),
+  color: randomColor() // Fix: Call the randomColor function to get a random color
+}
+
+let direction, loopId
+
+const drawFood = () => {
+  const { x, y, color } = food
+  ctx.shadowColor = color
+  ctx.shadowBlur = 6
+  ctx.fillStyle = color
+  ctx.fillRect(x, y, size, size) // Fix: Use fillRect instead of fillReact
+  ctx.shadowBlur = 0
+}
 
 const drawSnake = () => {
   ctx.fillStyle = '#ddd'
@@ -40,9 +81,106 @@ const moveSnake = () => {
   snake.shift()
 }
 
-setInterval(() => {
-  ctx.clearRect(0, 0, 600, 600)
+const drawGrid = () => {
+  ctx.lineWidth = 1
+  ctx.strokeStyle = '#191919'
 
+  for (let i = 30; i < canvas.width; i += 30) {
+    ctx.beginPath()
+    ctx.lineTo(i, 0)
+    ctx.lineTo(i, canvas.height) // Fix: Use canvas.height instead of hardcoded value
+    ctx.stroke()
+
+    ctx.beginPath()
+    ctx.lineTo(0, i)
+    ctx.lineTo(canvas.width, i) // Fix: Use canvas.width instead of hardcoded value
+    ctx.stroke()
+  }
+}
+
+const checkEat = () => {
+  const head = snake[snake.length - 1]
+
+  if (head.x == food.x && head.y == food.y) {
+    incrementScore()
+    snake.push(head)
+    audio.play()
+
+    let x = randomPosition()
+    let y = randomPosition()
+
+    while (snake.find(position => position.x == x && position.y == y)) {
+      x = randomPosition()
+      y = randomPosition()
+    }
+
+    food.x = x
+    food.y = y
+    food.color = randomColor()
+  }
+}
+
+const checkCollision = () => {
+  const head = snake[snake.length - 1]
+  const canvasLimit = canvas.width - size
+  const neckIndex = snake.length - 2
+  const wallCollision =
+    head.x < 0 || head.x > canvasLimit || head.y < 0 || head.y > canvasLimit
+
+  const selfCollision = snake.find((position, index) => {
+    return index < neckIndex && position.x == head.x && position.y == head.y
+  })
+
+  if (wallCollision || selfCollision) {
+    gameOver()
+  }
+}
+
+const gameOver = () => {
+  direction = undefined
+
+  menu.style.display = 'flex'
+  finalScore.innerText = score.innerText
+  canvas.style.filter = 'blur(2px)'
+}
+
+const gameLoop = () => {
+  clearInterval(loopId)
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height) // Fix: Use canvas.width and canvas.height instead of hardcoded values
+  drawGrid()
+  drawFood()
   moveSnake()
   drawSnake()
-}, 300)
+  checkEat()
+  checkCollision()
+
+  loopId = setTimeout(() => {
+    gameLoop()
+  }, 300)
+}
+
+gameLoop()
+
+document.addEventListener('keydown', ({ key }) => {
+  if (key == 'ArrowRight' && direction != 'left') {
+    direction = 'right'
+  }
+  if (key == 'ArrowLeft' && direction != 'right') {
+    direction = 'left'
+  }
+  if (key == 'ArrowDown' && direction != 'up') {
+    direction = 'down'
+  }
+  if (key == 'ArrowUp' && direction != 'down') {
+    direction = 'up'
+  }
+  console.log(key)
+})
+buttonPlay.addEventListener('click', () => {
+  score.innerText = '00'
+  menu.style.display = 'none'
+  canvas.style.filter = 'none'
+  snake = [initialPosition]
+  gameLoop() // Add this line to start the game loop after clicking the play button
+})
